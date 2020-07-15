3440b889d738ba8d96d2e287dbcdfee1c26a3401
Chase Coleman <60491004+cacoleman16@users.noreply.github.com>
2020-07-15 15:35:36 -0400

    Update update_all_data.py

--- update_all_data.py
+++ update_all_data.py
@@ -1,8 +1,12 @@
-
 import download_libor_data
 import download_oil_futures
+import opec.get_opec
 
 path = "" # Put your path to save the data here
 
+
+
+
 download_oil_futures.main()
 download_libor_data.main()
+opec.get_opec.main()
