bd0a12130b1fa3b0ba44e6e2eedfbecc54065fe8
Chase Coleman <60491004+cacoleman16@users.noreply.github.com>
2020-07-16 12:03:54 -0400

    Update download_oil_futures.py

--- download_oil_futures.py
+++ download_oil_futures.py
@@ -4,7 +4,12 @@ import os
 import quandl
 import apiKey
 
+
+# Variables
 key = apiKey.QUANDL_API_KEY
+number_of_futures = 38  # This determines how many contracts of cl to downloand. max is 38
+file_name = "oilfutures.xlsx"  # sets the file name
+
 
 # temporary program to download oil futures and save as xls file
 
@@ -12,13 +17,18 @@ key = apiKey.QUANDL_API_KEY
 
 
 def main():
-    oilfutures = quandl.get("CHRIS/CME_CL1.6", authtoken=key)
-    oilfutures = oilfutures.rename(columns={'Settle': 'CL1Settle'})
-    for i in range(2, 21):
-        c = quandl.get("CHRIS/CME_CL" + str(i) + ".6", authtoken=key)
-        c = c.rename(columns={'Settle': 'CL' + str(i) + 'Settle'})
-        oilfutures = oilfutures.join(c)
-    oilfutures.to_excel(path + "oilfutures.xlsx", na_rep='NaN')
+    try:
+        oilfutures = quandl.get("CHRIS/CME_CL1.6", authtoken=key)
+        oilfutures = oilfutures.rename(columns={'Settle': 'CL1Settle'})
+        for i in range(2, number_of_futures + 1):
+            c = quandl.get("CHRIS/CME_CL" + str(i) + ".6", authtoken=key)
+            c = c.rename(columns={'Settle': 'CL' + str(i) + 'Settle'})
+            oilfutures = oilfutures.join(c)
+        oilfutures.to_excel(
+            apiKey.path + file_name, na_rep='NaN')
+
+    except:
+        print("There was a problem retreiving the data. Conact chasecoleman@uky.edu to update the code")
 
 
 if __name__ == "__main__":
